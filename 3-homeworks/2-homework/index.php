387a36d66cdd65ec0040fdf1f579cc60e15238eb
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2022-01-08 12:07:19 +0200

    Add files via upload

--- 3-homeworks/2-homework/index.php
+++ 3-homeworks/2-homework/index.php
@@ -1,3 +1,9 @@
+<?php 
+	$standart_value = 1;
+	
+	if (isset($_POST['month']))
+		$standart_value = $_POST['month'];
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,95 +12,132 @@
 	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 	    <style type="text/css">
-	    	button {
-	    		margin-top: 20px; 
-	    		width: 190px;
-	    	}
-
-	    	body {
-	    		background: rgb(23,10,247);
-				background: linear-gradient(90deg, rgba(23,10,247,1) 0%,
-							rgba(9,9,121,1) 35%, 
-							rgba(0,212,255,1) 100%);
-	    	}
-	    	
-	    	h3 {
-	    		color: white;
-	    	}
-
-	    	.sf {
-	    		margin: 20px;
-	    		background-color: white;
-	    		height: 200px;
-	    		display: flex; /* establish flex container */
-			    justify-content: center; /* center flex items horizontally, in this case */
-			    align-items: center; /* center flex items vertically, in this case */
-	    	}
-
-	    	input {
-	    		width: 190px;
-	    	}
+	 		.dayOff {
+	 			background-color: green;
+	 		}
 	    </style>
 	</head>
 
 	<body>
 		<center>
 
-
-			<form id="form1" action="index.php" method='POST'>
+			<form id="form" action="index.php" method='POST'>
 				
 				<div class="container">
 				  	
 				  	<div class="row justify-content-around">
-					    <div class="col-3 align-self-start">
-					    	<h3>Red</h3>
-					       	<input type="number" name="Red" value="Red" min="0" max="255">
+					    <div class="col-5 align-self-start">
+					    	<h3>Enter month(number from 1 to 12)</h3>
+					       	<input type="number" name="month" value="<?= $standart_value ?>" min="1" max="12">
 				    	</div>
 				    </div>
-				  
-				  	<div class="row justify-content-around">
-				  		<div class="col-3 align-self-center">
-				     		<h3>Green</h3>
-				     		<input type="number" name="Green" value="Green" min="0" max="255">
-				  		</div>
-				  	</div>
-
-					<div class="row justify-content-around">
-					  	 <div class="col-3 align-self-end">
-					    	 <h3>Blue</h3>
-					    	 <input type="number" name="Blue" value="Blue" min="0" max="255">
-					    </div>
-				 	</div>
-
-					<div class="row justify-content-around">
-					  	<div class="col-3 align-self-end">
-					    	<button type="submit" value="send" class="btn btn-success" id="submit">Accept</button>
-					    </div>
-					</div>
+			
 				</div>
 
+				<br>
+				<input type="submit" name="" value="Show">
+	
 			</form>
 		</center>
 	</body>
 </html>
 
 <?php
-	if (((isset($_POST['Red']) && isset($_POST['Green']) && isset($_POST['Blue']))))
-	{			
-		$red_value = $_POST['Red'];
-		$green_value = $_POST['Green'];
-		$blue_value = $_POST['Blue'];
-
-		echo "<div>
-				<center>
-					<h5 style=\"color: white; margin-top: 20px; \">
-						rgb($red_value, $green_value, $blue_value):
-					</h5>
-				</center>
-			 </div>";
+	function drawCalendar($month)
+	{
+		$i=0;
+		print('<table border=1><tr>');
+		$n = array('1'=>'Понедельник','2'=>'Вторник','3'=>'Среда','4'=>'Четверг','5'=>'Пятница','6'=>'Суббота','7'=>'Восресенье');
 		
-		echo "<span style=\"background-color: rgb($red_value, $green_value, $blue_value); \" class=\"sf\">
-					Full he none no side. Uncommonly surrounded considered for him are its. It we is read good soon. My to considered delightful invitation announcing of no decisively boisterous. Did add dashwoods deficient man concluded additions resources. Or landlord packages overcame distance smallest in recurred. Wrong maids or be asked no on enjoy. Household few sometimes out attending described. Lain just fact four of am meet high.
-			 </span>";
+		do{
+		    $i++;
+		    echo '<td>'.$n[$i].' </td>';
+		}while($i < count($n));
+		echo '</tr><tr>';
+		
+		$day=1;
+		switch($month)
+		{
+			case 1: 
+				$day = 6;
+				break;
+			case 2:
+				$day = 2;
+				break;
+			case 3:
+ 				$day = 2;
+				break;
+		    case 4:
+		        $day = 5;
+		        break;
+		    case 5:
+		    	$day = 7;
+		    	break;
+		    case 6:
+		    	$day = 3;
+		    	break;
+			case 7:
+		    	$day = 5;
+		    	break;
+   			case 8:
+		    	$day = 1;
+		    	break;
+   			case 9:
+		    	$day = 4;
+		    	break;
+   			case 10:
+		    	$day = 4;
+		    	break;
+   			case 11:
+		    	$day = 2;
+		    	break;
+   			case 12:
+		    	$day = 4;
+		}
+
+		if ($day > 1)
+		{
+			for ($i = 1; $i != $day; $i++)
+			{
+				echo "<td>  </td>";
+			}
+		}
+		$countDaysInMonth = date('t', mktime(0, 0, 0, $month, 1, 2022));
+
+		for($i = 1; $i < $countDaysInMonth + 1; $i++)
+		{
+		    if($i == date('d') && $month == date('n')) {
+		         echo '<td bgcolor=black><font color=white><b>' . $i . '</b></font></td>';
+		    } else {
+		        if ($day > 7)
+		        {
+		          $day=1;
+		        }
+
+		        $date = date('Y-n')."-".$i;
+		        
+		        $now_day = (strftime("%w", strtotime($date))==0) ? 7 : strftime("%w", strtotime($date));
+		        
+		        if ($day == 6 || $day == 7)
+		        {
+		        	echo '<td class="dayOff"> ' . $i . ' </td>';
+		        }
+		        else
+		        {
+		        	echo "<td>" . $i . " </td>";
+		        }		        
+		    }
+
+		    if($day == 7)
+		    	echo '</tr>';
+		    $day++;
+		}
+		echo '</tr><table>';
+	};
+
+	if ((isset($_POST['month'])))
+	{			
+		drawCalendar($_POST['month']);
 	}
+
 ?>
