bfc97612e794490486dae01920a550dd3d6ae41a
Ivan Savin <68004563+SoV3n04ek@users.noreply.github.com>
2022-01-18 23:21:27 +0200

    updated IModel.php

--- 3-homeworks/homework4/IModel.php
+++ 3-homeworks/homework4/IModel.php
@@ -1,6 +1,12 @@
 <?php
 	interface IModel
 	{
+		public function insert();
+		public function update();
+		public function readAll();
+		public function readOne();
+		public function delete();
+		
 	    public function connect();
 	    public function close();
 	}
